dbfc250a8cc0aad41fdf48bc1071ecfc4f9211fa
Doug Whitmore <dougwhitmore@mac.com>
2016-03-04 16:06:47 -0800

    Initial Commit

---
+++ LRUCache/LRUCache.h
@@ -0,0 +1,19 @@
+//
+//  LRUCache.h
+//  LRUCache
+//
+//  Created by Doug Whitmore on 3/4/16.
+//  Copyright © 2016 Good Doug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LRUCache.
+FOUNDATION_EXPORT double LRUCacheVersionNumber;
+
+//! Project version string for LRUCache.
+FOUNDATION_EXPORT const unsigned char LRUCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LRUCache/PublicHeader.h>
+
+
